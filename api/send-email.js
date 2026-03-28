f2f365204061dc2406e19143104dc9c3b5be2fcc
jiytrue <betterstreetsph@gmail.com>
2026-03-28 14:49:33 +0800

    Fix Vercel serverless function crashing due to CommonJS require

--- api/send-email.js
+++ api/send-email.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 // All staff email mapping
 const TECH_EMAILS = {
@@ -27,7 +27,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
